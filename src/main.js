5aca39c7f90c093b86ec4c648c496773b8c4d0f8
cobaltgit <cobaltsecuremail312@proton.me>
2025-12-31 12:21:36 +0000

    YouTube url validation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { invoke } from "@tauri-apps/api/core";
 import { open, message } from "@tauri-apps/plugin-dialog";
 import { listen } from "@tauri-apps/api/event";
 
+const YouTubeRegex =
+  /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+
 await listen("download-progress", (event) => {
   const progress = event.payload;
   document.getElementById("download-progress").value = progress;
@@ -21,6 +24,14 @@ document.getElementById("download").addEventListener("submit", async (e) => {
   const url = formData.get("url");
   const quality = formData.get("quality");
   const filter = formData.get("filter");
+
+  if (!YouTubeRegex.test(url)) {
+    await message("Please enter a valid YouTube URL", {
+      title: "Error",
+      kind: "error",
+    });
+    return;
+  }
 
   const file_path = await open({
     directory: true,
